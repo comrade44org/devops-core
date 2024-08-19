0c4813bdbab31a2c19bde9876d53b8320b991cce
Richard McGinn <mcginnr@gmail.com>
2024-08-19 15:29:17 +0100

    Add storage account Terraform

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -2,3 +2,16 @@ resource "azurerm_resource_group" "rg-devops-01" {
   name     = "rg-devops-01"
   location = "uksouth"
 }
+
+resource "azurerm_storage_account" "stgterraformastate44" {
+  name = "stgterraformstate44"
+  resource_group_name = azurerm_resource_group.rg-devops-01
+  location = "uksouth"
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "stg-tfstate-01" {
+  name = "stg-tfstate-01"
+  storage_account_name = azurerm_storage_account.stgterraformastate44.name
+}
