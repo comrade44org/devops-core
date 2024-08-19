06140748a97bd592c932d4b6d0ac926ed4924d18
Richard McGinn <mcginnr@gmail.com>
2024-08-19 15:31:24 +0100

    Fix name reference

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg-devops-01" {
 
 resource "azurerm_storage_account" "stgterraformastate44" {
   name = "stgterraformstate44"
-  resource_group_name = azurerm_resource_group.rg-devops-01
+  resource_group_name = azurerm_resource_group.rg-devops-01.name
   location = "uksouth"
   account_tier = "Standard"
   account_replication_type = "LRS"
